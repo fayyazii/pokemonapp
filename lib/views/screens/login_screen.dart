b671d994cb7d2d615e5031287330a695c1a7b55e
fahad shabir <fadiktk005@gmail.com>
2023-10-22 19:22:50 +0500

    pokeman app

--- lib/views/screens/login_screen.dart
+++ lib/views/screens/login_screen.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pokedex/cubits/signin_cubit/signin_cubit.dart';
+import 'package:pokedex/cubits/signup_cubit/signup_cubit.dart';
 import 'package:pokedex/views/screens/root_screen/pages/home_screen.dart';
+import 'package:pokedex/views/screens/root_screen/root_screen.dart';
+import 'package:pokedex/views/screens/signup_screen.dart';
 
 class LoginScreeen extends StatefulWidget {
   const LoginScreeen({Key? key}) : super(key: key);
@@ -17,6 +20,7 @@ class _LoginScreeenState extends State<LoginScreeen> {
   final txtEmail = TextEditingController();
 
   final txtPass = TextEditingController();
+  final _formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -24,102 +28,166 @@ class _LoginScreeenState extends State<LoginScreeen> {
       appBar: AppBar(
         title: const Text("Login Screen"),
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 20.sp,
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 22.sp),
-            child: const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Email",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15),
-              ),
+      body: Form(
+        key: _formkey,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20.sp,
             ),
-          ),
-          SizedBox(
-            height: 8.sp,
-          ),
-          // Note: Same code is applied for the TextFormField as well
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.sp),
-            child: TextField(
-              controller: txtEmail,
-              decoration: InputDecoration(
-                hintText: "Enter Email",
-                enabledBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    width: 1, //<-- SEE HERE
-                    color: Color(0xffACACAC),
-                  ),
-                  borderRadius: BorderRadius.circular(50.0),
+            Padding(
+              padding: EdgeInsets.only(left: 22.sp),
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Email",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 20.sp,
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 22.sp),
-            child: const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Password",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15),
-              ),
+            SizedBox(
+              height: 8.sp,
             ),
-          ),
-          SizedBox(
-            height: 8.sp,
-          ),
-          // Note: Same code is applied for the TextFormField as well
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.sp),
-            child: TextField(
-              controller: txtPass,
-              decoration: InputDecoration(
-                hintText: "Enter Password",
-                enabledBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    width: 1, //<-- SEE HERE
-                    color: Color(0xffACACAC),
+            // Note: Same code is applied for the TextFormField as well
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.sp),
+              child: TextFormField(
+                validator: (value){
+                  if (value==null)
+                  {
+                    return "field cannot be empty";
+                  }
+                },
+                controller: txtEmail,
+                decoration: InputDecoration(
+                  hintText: "Enter Email",
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      width: 1, //<-- SEE HERE
+                      color: Color(0xffACACAC),
+                    ),
+                    borderRadius: BorderRadius.circular(50.0),
                   ),
-                  borderRadius: BorderRadius.circular(50.0),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 20.sp,
-          ),
+            SizedBox(
+              height: 20.sp,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 22.sp),
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Password",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 8.sp,
+            ),
+            // Note: Same code is applied for the TextFormField as well
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.sp),
+              child: TextFormField(
+                validator: (value){
+                  if (value==null)
+                    {
+                      return "field cannot be empty";
+                    }
+                },
+                controller: txtPass,
+                decoration: InputDecoration(
+                  hintText: "Enter Password",
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      width: 1, //<-- SEE HERE
+                      color: Color(0xffACACAC),
+                    ),
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20.sp,
+            ),
 
 
 
-          BlocListener<SigninCubit, SigninState>(
-            listener: (context, state) {
-              // TODO: implement listener
-              if(state is SigninSuccess){
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(builder: (context)=>HomeScreen()),
-                        (route) => false);
-              }
-              if(state is SigninFailed){
-                log(state.err);
-              }
-            },
-            child: GestureDetector(
-              onTap: () {
-                context.read<SigninCubit>().login(txtEmail.text, txtPass.text);
+            BlocListener<SigninCubit, SigninState>(
+              listener: (context, state) {
+                if (state is SignupError) {
+                  showDialog(
+                    context: context,
+                    builder: (context) =>  AlertDialog(
+                      content: SizedBox(
+                        height: 100.h,
+                        child:const  Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      ),
+                    ),
+                  );
+                }
+
+                // TODO: implement listener
+                if(state is SigninSuccess){
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context)=>RootScreen()),
+                          (route) => false);
+                }
+                if(state is SigninFailed){
+                  //Navigator.pop(context);
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text(state.err),
+                    backgroundColor: Colors.red,
+                  ));
+                  log(state.err);
+                }
+              },
+              child: GestureDetector(
+                onTap: () {
+                  if(_formkey.currentState!.validate())
+                    {
+                      context.read<SigninCubit>().login(txtEmail.text, txtPass.text);
+                    }
+
+
+                },
+                child: Container(
+                  height: 40.sp,
+                  width: 272.sp,
+                  decoration: BoxDecoration(
+                    //gradient: Constants.gradient,
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(20.sp)),
+                  child: const Center(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20.sp,
+            ),
+            InkWell(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>SignupScreen()));
               },
               child: Container(
                 height: 40.sp,
@@ -130,7 +198,7 @@ class _LoginScreeenState extends State<LoginScreeen> {
                     borderRadius: BorderRadius.circular(20.sp)),
                 child: const Center(
                   child: Text(
-                    "Signin",
+                    "Signup",
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 16,
@@ -139,29 +207,9 @@ class _LoginScreeenState extends State<LoginScreeen> {
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 20.sp,
-          ),
-          Container(
-            height: 40.sp,
-            width: 272.sp,
-            decoration: BoxDecoration(
-              //gradient: Constants.gradient,
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(20.sp)),
-            child: const Center(
-              child: Text(
-                "Signup",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                    color: Colors.white),
-              ),
-            ),
-          ),
 
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/views/screens/signup_screen.dart
+++ lib/views/screens/signup_screen.dart
@@ -14,162 +14,187 @@ class SignupScreen extends StatefulWidget {
 class _SignupScreenState extends State<SignupScreen> {
   final txtEmail=TextEditingController();
   final txtPass=TextEditingController();
+  final _formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Login Screen"),),
-      body: Column(
-        children: [
-          SizedBox(height: 20.sp,),
-          Padding(
-            padding: EdgeInsets.only(left: 22.sp),
-            child: const Align(
-              alignment: Alignment.centerLeft,
-              child: Text("Name", style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15
-              ),),
-            ),
-          ),
-          SizedBox(height: 8.sp,),
-          // Note: Same code is applied for the TextFormField as well
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.sp),
-            child: TextField(
-
-              decoration: InputDecoration(
-                hintText: "Enter Name",
-                enabledBorder: OutlineInputBorder(
-                  borderSide:
-                  const BorderSide(
-                    width: 1, //<-- SEE HERE
-                    color: Color(0xffACACAC),),
-                  borderRadius: BorderRadius.circular(50.0),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 20.sp,),
-          Padding(
-            padding: EdgeInsets.only(left: 22.sp),
-            child: const Align(
-              alignment: Alignment.centerLeft,
-              child: Text("Email", style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15
-              ),),
-            ),
-          ),
-          SizedBox(height: 8.sp,),
-          // Note: Same code is applied for the TextFormField as well
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.sp),
-            child: TextField(
-              controller: txtPass,
-              decoration: InputDecoration(
-                hintText: "Enter Email",
-                enabledBorder: OutlineInputBorder(
-                  borderSide:
-                  const BorderSide(
-                    width: 1, //<-- SEE HERE
-                    color: Color(0xffACACAC),),
-                  borderRadius: BorderRadius.circular(50.0),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 20.sp,),
-          Padding(
-            padding: EdgeInsets.only(left: 22.sp),
-            child: const Align(
-              alignment: Alignment.centerLeft,
-              child: Text("Password", style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15
-              ),),
-            ),
-          ),
-          SizedBox(height: 8.sp,),
-          // Note: Same code is applied for the TextFormField as well
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.sp),
-            child: TextField(
-              controller: txtPass,
-              decoration: InputDecoration(
-                hintText: "Enter Password",
-                enabledBorder: OutlineInputBorder(
-                  borderSide:
-                  const BorderSide(
-                    width: 1, //<-- SEE HERE
-                    color: Color(0xffACACAC),),
-                  borderRadius: BorderRadius.circular(50.0),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 20.sp,),
-
-          BlocListener<SignupCubit, SignupState>(
-            listener: (context, state) {
-              // TODO: implement listener
-              if(state is SignupSuccess){
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(builder: (context)=>LoginScreeen()),
-                        (route) => false);
-              }
-            },
-            child: GestureDetector(
-              onTap: () {
-                context.read<SignupCubit>().signup(txtEmail.text, txtPass.text);
-              },
-              child: Container(
-                height: 40.sp,
-                width: 272.sp,
-
-                decoration: BoxDecoration(
-                  //gradient: Constants.gradient,
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(20.sp)
-                ),
-                child: const Center(
-                  child: Text("Signup",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        color: Colors.white
-                    ),),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 20.sp,),
-          Container(
-            height: 40.sp,
-            width: 272.sp,
-
-            decoration: BoxDecoration(
-              //gradient: Constants.gradient,
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(20.sp)
-            ),
-            child: const Center(
-              child: Text("Login",
-                style: TextStyle(
+      body: Form(
+        key: _formkey,
+        child: ListView(
+          children: [
+            SizedBox(height: 20.sp,),
+            Padding(
+              padding: EdgeInsets.only(left: 22.sp),
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text("Name", style: TextStyle(
+                    color: Colors.black,
                     fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                    color: Colors.white
+                    fontSize: 15
                 ),),
+              ),
             ),
-          ),
-          SizedBox(height: 20.sp,),
+            SizedBox(height: 8.sp,),
+            // Note: Same code is applied for the TextFormField as well
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.sp),
+              child: TextFormField(
+                validator: (value){
+                  if (value==null)
+                  {
+                    return "field cannot be empty";
+                  }
+                },
+                decoration: InputDecoration(
+                  hintText: "Enter Name",
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                    const BorderSide(
+                      width: 1, //<-- SEE HERE
+                      color: Color(0xffACACAC),),
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.sp,),
+            Padding(
+              padding: EdgeInsets.only(left: 22.sp),
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text("Email", style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15
+                ),),
+              ),
+            ),
+            SizedBox(height: 8.sp,),
+            // Note: Same code is applied for the TextFormField as well
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.sp),
+              child: TextFormField(
+                validator: (value){
+                  if (value==null)
+                  {
+                    return "field cannot be empty";
+                  }
+                },
+                controller: txtEmail,
+                decoration: InputDecoration(
+                  hintText: "Enter Email",
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                    const BorderSide(
+                      width: 1, //<-- SEE HERE
+                      color: Color(0xffACACAC),),
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.sp,),
+            Padding(
+              padding: EdgeInsets.only(left: 22.sp),
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text("Password", style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15
+                ),),
+              ),
+            ),
+            SizedBox(height: 8.sp,),
+            // Note: Same code is applied for the TextFormField as well
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.sp),
+              child: TextFormField(
+                validator: (value){
+                  if (value==null)
+                  {
+                    return "field cannot be empty";
+                  }
+                },
+                controller: txtPass,
+                decoration: InputDecoration(
+                  hintText: "Enter Password",
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                    const BorderSide(
+                      width: 1, //<-- SEE HERE
+                      color: Color(0xffACACAC),),
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.sp,),
+
+            BlocListener<SignupCubit, SignupState>(
+              listener: (context, state) {
+                // TODO: implement listener
+                if(state is SignupSuccess){
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context)=>LoginScreeen()),
+                          (route) => false);
+                }
+              },
+              child: GestureDetector(
+                onTap: () {
+                  if(_formkey.currentState!.validate())
+                    {
+                      context.read<SignupCubit>().signup(txtEmail.text, txtPass.text);
+                    }
+
+                },
+                child: Container(
+                  height: 40.sp,
+                  width: 272.sp,
+
+                  decoration: BoxDecoration(
+                    //gradient: Constants.gradient,
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(20.sp)
+                  ),
+                  child: const Center(
+                    child: Text("Signup",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.white
+                      ),),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.sp,),
+            Container(
+              height: 40.sp,
+              width: 272.sp,
+
+              decoration: BoxDecoration(
+                //gradient: Constants.gradient,
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20.sp)
+              ),
+              child: const Center(
+                child: Text("Login",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Colors.white
+                  ),),
+              ),
+            ),
+            SizedBox(height: 20.sp,),
 
 
-        ],
+          ],
+        ),
       ),
     );
   }
